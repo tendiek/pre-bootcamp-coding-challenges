e495228888613a42d4fb50b11a8199705e7d3c29
tendiek <74530894+tendiek@users.noreply.github.com>
2020-11-21 20:32:58 +0200

    task 2 commit

---
+++ task2.py
@@ -0,0 +1,8 @@
+#Convert this pseudocode into actual code
+#Understanding BODMAS
+x = 1 + 1 * 2
+y = (1 + 1) * 2
+z = 1 + ( 1 * 2 )
+a =  1 + 1 * 2 / 2
+b =  (1 + 1 * 2 ) /  2
+print(x, y, z, a, b, sep='\n')
