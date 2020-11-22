e622c6d2460305bf25c0cc5fca7c7044dbf595a2
tendiek <74530894+tendiek@users.noreply.github.com>
2020-11-22 09:24:31 +0200

    added comment to task 3

--- task3.py
+++ task3.py
@@ -1,8 +1,9 @@
+# function to determine if a number is equals 65 or the 
+# sum of two numbers equals 65
 def my_func( x , y ):
     if (x == 65 or y == 65 or x + y == 65):
         return True
     else:
-       return False
-    
+       return False  
 
 print(my_func(55, 10))
