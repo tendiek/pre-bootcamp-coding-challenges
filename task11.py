6df96b72b4469f5a907a455043968597790775b5
tendiek <74530894+tendiek@users.noreply.github.com>
2020-11-23 23:05:31 +0200

    task 11 commit

---
+++ task11.py
@@ -0,0 +1,17 @@
+#function that takes two strings as input, and outputs the
+#  common characters/letters that they share.
+def letter_count(sentence1, sentence2):
+    count = 0
+    same_letters =[]
+    
+    sentence1 = sentence1.casefold()
+    sentence2 = sentence2.casefold()
+    for letter in sentence1:
+        if letter in sentence2:
+            same_letters.append(letter)
+            count += 1
+    print("Letters that appear in both sentences:",same_letters)
+
+sentence1 = input("Enter a sentence: ")
+sentence2 = input("Enter another sentence: ")
+letter_count(sentence1, sentence2)
