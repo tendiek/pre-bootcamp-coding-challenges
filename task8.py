e2e68a3535a99bc4f513a133e50d45757e12c87c
tendiek <74530894+tendiek@users.noreply.github.com>
2020-11-28 14:24:46 +0200

    fixing hour and hours to reflect correctly

--- task8.py
+++ task8.py
@@ -3,10 +3,17 @@
 def num_to_time(number):
     hours = number // 60
     minutes = number % 60
-    print(str(number) + " = " + str(hours) + " hour, " + str(minutes) + " minutes")
+    if hours <= 1 and minutes <=1 :
+        print(str(number) + " = " + str(hours) + " hour, " + str(minutes) + " minute")
+    elif hours <= 1 and minutes >1:
+        print(str(number) + " = " + str(hours) + " hour, " + str(minutes) + " minutes")
+    elif hours > 1 and minutes <=1:
+        print(str(number) + " = " + str(hours) + " hours, " + str(minutes) + " minute")
+    else:
+        print(str(number) + " = " + str(hours) + " hours, " + str(minutes) + " minutes")
     return
 
-print("Program to convert numbera to time units. ")
+print("Program to convert numbers to time units. ")
 number = int(input("Please enter number: "))
 num_to_time(number)
 
