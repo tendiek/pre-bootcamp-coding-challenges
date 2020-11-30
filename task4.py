dad7fc91f98a2a7bc69240b6fb3295064ebc9644
tendiek <74530894+tendiek@users.noreply.github.com>
2020-12-01 01:12:28 +0200

    Update task4.py
    changed function name

--- task4.py
+++ task4.py
@@ -1,6 +1,6 @@
 # function that takes two numbers as input and determine if either of the numbers is 3 
 # AND the sum of the numbers contains a 3.
-def my_func( x , y ):
+def num_compare( x , y ):
     add =x + y
     if (x == 3 or y == 3) and ("3" in str(add)):
         return True
@@ -9,4 +9,4 @@ def my_func( x , y ):
 
 x = int(input("Please enter 1st number: "))
 y = int(input("Please enter 2nd number: "))
-print(my_func(x ,y ))
+print(num_compare(x ,y ))
