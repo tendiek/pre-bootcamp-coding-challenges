0a7a5132d408352cd547327871f9b4f8f8688adf
tendiek <74530894+tendiek@users.noreply.github.com>
2020-12-01 01:21:34 +0200

    Update task10.py
    removed unused variable

--- task10.py
+++ task10.py
@@ -1,6 +1,5 @@
 #function that takes in a string and then prints out all the vowels in the string
 def vowel_count(sentence):
-    count = 0
     my_vowels =[]
     vowels ="aeiou"
     
@@ -8,7 +7,6 @@ def vowel_count(sentence):
     for letter in sentence:
         if letter in vowels:
             my_vowels.append(letter)
-            count += 1
     print(my_vowels)
 
 sentence = input("Enter a sentence: ")
